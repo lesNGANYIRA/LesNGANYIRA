8548c2493d2baa66ee1a2e0ab030e558cdeaa1e3
lesNGANYIRA <brucevictorirakoze@gmail.com>
2025-08-12 23:31:12 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,6 @@ exports.ping = onRequest((req, res) => {
   if (req.method === 'OPTIONS') return res.status(204).send('');
   res.status(200).send('OK from Firebase Functions');
 });
+exports.createCheckout = onRequest({ secrets: [STRIPE_SECRET] }, async (req, res) => { /* … */ });
+exports.stripeWebhook  = onRequest({ secrets: [STRIPE_SECRET, STRIPE_WH_SECRET] }, async (req, res) => { /* … */ });
+
